83c357951416de73f264e4cf4c0a295a4bfc56b2
Ram Ramani <rrnara@hotmail.com>
2019-11-21 18:05:09 -0500

    Minor cleanup

--- client/components/Actions/Requests.js
+++ client/components/Actions/Requests.js
@@ -1,5 +1,5 @@
 export const CLEAR_REQUEST_STATE = `CLEAR_REQUEST_STATE`;
 
 export const clearRequestState = (httpMethod, entityName, suffxName = undefined) => {
-  return { type: LOGOUT_EVENT, payload: { httpMethod, entityName, suffxName } };
+  return { type: CLEAR_REQUEST_STATE, payload: { httpMethod, entityName, suffxName } };
 };
